2e238a5d2b9dca76481156563e3bb228c399c34a
laksh vyas <2023pietadlaksh033@poornima.org>
2026-01-04 15:09:30 +0530

    Linked List

---
+++ src/LL.java
@@ -0,0 +1,55 @@
+public class LL {
+    Node head;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    public void printList() {
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.addFirst("is");
+        list.addFirst("a");
+        list.addLast("list");
+        list.addFirst("this");
+        list.printList();
+    }
+}
