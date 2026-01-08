828e182afaad2baa3f398653a31726a240fd9297
laksh vyas <2023pietadlaksh033@poornima.org>
2026-01-08 23:04:40 +0530

    Factorial

---
+++ src/factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class factorial {
+    public static int calcFact(int n){
+        if(n == 1 || n == 0){
+            return 1;
+        }
+        int fact_nm1 = calcFact(n-1);
+        int fact_n = n * fact_nm1;
+        return fact_n;
+    }
+    public static void main(String[] args) {
+        System.out.println(calcFact(5));
+    }
+}
