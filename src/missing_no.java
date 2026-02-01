7ae5636476212a080b042832733e0aa9e9d73bd8
laksh vyas <2023pietadlaksh033@poornima.org>
2026-02-01 15:28:20 +0530

    missing no in an array two approaches

--- src/missing_no.java
+++ src/missing_no.java
@@ -1,4 +1,8 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
+
+/* APPROACH 1 ( TIME COMPLEXITY = O(N^2))
 
 public class missing_no {
     public static void main(String[] args) {
@@ -28,5 +32,30 @@ public class missing_no {
                 break;
             }
         }
+    }
+}
+*/
+
+/*
+APPROACH 2
+ */
+public class missing_no {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int len = arr.length;
+        System.out.println("enter elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int expected_sum = n * (n + 1) / 2;
+        int actual_sum = 0;
+        for (int i = 0; i < len; i++) {
+            actual_sum += arr[i];
+        }
+        int missing = expected_sum - actual_sum;
+        System.out.println("missing number is : "+ missing);
     }
 }
