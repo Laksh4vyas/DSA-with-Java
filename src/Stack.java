72d6127120f0ccdb09601b9ccf5631a9e4ab387c
laksh vyas <2023pietadlaksh033@poornima.org>
2026-03-03 17:07:47 +0530

    Stack implementation using linkedlist

---
+++ src/Stack.java
@@ -0,0 +1,73 @@
+public class Stack {
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class stack {
+        public static Node head;
+
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        public static void push(int data) {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+
+        public static void display() {
+            if (isEmpty()) {
+                System.out.println("Stack is empty");
+                return;
+            }
+
+            Node temp = head;
+            while (temp != null) {
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        stack s = new stack();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        System.out.println("Stack elements:");
+        s.display();
+
+        System.out.println("Popping elements:");
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
